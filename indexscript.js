c2183245d9ad6f47958a8f6f4f2cc8ca3762b1bb
unknown <amoled27>
2020-01-16 18:42:02 +0530

    dshd

--- indexscript.js
+++ indexscript.js
@@ -8,8 +8,10 @@ sticky = header.offsetTop;
 function myFunction() {
     if (window.pageYOffset > sticky) {
         header.classList.add("pad-t1");
+        header.classList.add("navbar-small");
     } else {
         header.classList.remove("pad-t1");
+        header.classList.remove("navbar-small");
     }
 }
 
