a1ba28b60ce20d5b5743b1aa92cfe36b49797b92
unknown <amoled27>
2020-01-16 14:41:50 +0530

    remove topmot header

--- indexscript.js
+++ indexscript.js
@@ -2,13 +2,14 @@ window.onscroll = function () { myFunction() };
 
 var header = document.getElementById("theHeader");
 var sticky;
-    sticky = header.offsetTop;
+sticky = header.offsetTop;
 
 
 function myFunction() {
-        if (window.pageYOffset > sticky) {
-            header.classList.add("pad-t1");
-        } else {
-            header.classList.remove("pad-t1");
-        }
+    if (window.pageYOffset > sticky) {
+        header.classList.add("pad-t1");
+    } else {
+        header.classList.remove("pad-t1");
+    }
 }
+
