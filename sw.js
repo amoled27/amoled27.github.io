759bdd0fad6aecea44b1f8a77ca67cb98e79670b
amoled27 <amodshinde1995@gmail.com>
2022-01-03 10:35:27 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-135b076f0a52104abbea.js"
+    "url": "webpack-runtime-84c9b19545668f3a8b21.js"
   },
   {
     "url": "framework-b5b229d58c3c710d5429.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b8f8e57a871f49ff54ab7b4e4ed6062e"
+    "revision": "a32237e5e8d3b24ba2514612bc955585"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
